b21f47851cd95ae1b9d649d91fb38967fe426276
MichaelBG <michaelbekele28@gmail.com>
2022-03-21 14:39:26 +0300

    First Commit

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'root'; 
+$DatabaseName = 'os-new'; 
+$DatabasePort = '3306'; 
+?>
